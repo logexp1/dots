06b6305c972ef6467a2accadd2647ce01fa770b6
Jisoo Lee <jisoo.h.lee@gmail.com>
2026-04-30 11:53:05 +0900

    mini plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = 'm'
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 require 'config.options'
 require 'config.keymaps'
@@ -9,8 +9,12 @@ require 'config.autocmds'
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local out = vim.fn.system {
-    'git', 'clone', '--filter=blob:none', '--branch=stable',
-    'https://github.com/folke/lazy.nvim.git', lazypath,
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    'https://github.com/folke/lazy.nvim.git',
+    lazypath,
   }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'nvim-mini/mini.pairs',
+    event = 'VeryLazy',
+    opts = {},
+  },
+
+  {
+    'nvim-mini/mini.ai',
+    event = 'VeryLazy',
+    opts = {},
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
